9b0c13e3b5422d8c23a80b37214c05b778b4c686
Alejandro Duque <ducode@outlook.com>
2019-05-01 10:59:40 -0300

    users models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+"""users model"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+ROL_CHOICES = (
+    (1, 'Admin'),
+    (2, 'Consulta')
+)
+
+
+class Institution(models.Model):
+    """Institutions model"""
+    name = models.CharField('Nombre', max_length=30, blank=True)
+
+    def __str__(self):
+        """return name"""
+        return self.name
+
+
+class Profile(models.Model):
+    """Profile model that extens the db with other informations"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.PositiveIntegerField(
+        'Rol',
+        choices=ROL_CHOICES,
+    )
+    institution = models.OneToOneField(Institution, on_delete=models.CASCADE)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """return username"""
+        return self.user.username
--- users/views.py
+++ users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+""" Users view"""
 
-# Create your views here.
+# Django
+from django.contrib.auth import authenticate,login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import views as auth_views
+from django.shortcuts import render, redirect
+from django.views.generic import DetailView, FormView, UpdateView
+from django.urls import reverse_lazy
+
+# Models
+from django.contrib.auth.models import User
+from users.models import Profile
+from posts.models import *
+
+# Forms
+from users.forms import *
+
+
+class LoginView(auth_views.LoginView):
+    """Login view"""
+    template_name = 'users/login.html'
+
+
+class LogoutView(LoginRequiredMixin, auth_views.LogoutView):
+    """logout view"""
+    template_name = 'users/logout.html'
